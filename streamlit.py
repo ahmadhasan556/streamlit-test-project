0b19624db8f73b0f6e1ffc28dd70e9f68af0cb7c
ahmi5567 <ah55677435@gmail.com>
2025-12-29 22:07:36 +0500

    first commit of streamlit porject

---
+++ streamlit.py
@@ -0,0 +1,18 @@
+from google import genai
+import streamlit as st
+
+client = genai.Client(api_key='AIzaSyA3_ZUhquNtk8mT2yuSKERJrX3gpWKVVhY')
+
+
+st.title("Streamlit First App")
+st.write("This app demonstrates a conversational agent.")
+
+user_input = st.text_input("Ask anything")
+if st.button("Submit"):
+    with st.spinner("Agent is thinking..."):
+        response = client.models.generate_content(
+            model='gemini-2.5-flash', contents=user_input
+        )
+    st.write(response.text)
+
+#streamlit run 05-b-Streamlit.py
